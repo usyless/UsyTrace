55730668ea81fc2c3639d56c37418a235a049682
usy <contact.usyless@gmail.com>
2024-07-24 22:11:21 +0100

    update init file to keep new trace.js file

--- src/init.js
+++ src/init.js
@@ -1,7 +1,9 @@
 const js = document.createElement('script');
 if (window.localStorage.getItem('update') === 'true') {
-    js.setAttribute('src', 'trace.js?update');
+    const t = performance.timeOrigin.toString();
+    js.setAttribute('src', `trace.js?${t}`);
     window.localStorage.removeItem('update');
+    window.localStorage.setItem('trace', t);
 }
-else js.setAttribute('src', 'trace.js');
+else js.setAttribute('src', `trace.js${window.localStorage.getItem('trace')}`);
 document.body.appendChild(js);
