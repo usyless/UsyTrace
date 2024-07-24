124869d63547745155e444f238cdb733b2774f47
usy <contact.usyless@gmail.com>
2024-07-25 00:04:21 +0100

    update release builder

--- release-builder.py
+++ release-builder.py
@@ -1,4 +1,5 @@
 import shutil
+import filecmp
 from pathlib import Path
 from time import time
 
@@ -8,25 +9,20 @@ out_dir = main_dir / 'out'
 
 unix_time = str(int(time()))
 
-# Create 'Out' directory, or clear all items currently inside 'Out'
-if not out_dir.exists():
-    out_dir.mkdir()
-else:
-    for item in out_dir.iterdir():
-        try:
-            if item.is_file() or item.is_symlink():
-                item.unlink()
-        except Exception as e:
-            print(f'Failed to delete {item}. Reason: {e}')
+# Create 'Out' directory
+out_dir.mkdir(exist_ok=True)
+
+
+def copy_to_out_with_name(file: Path, name: str):
+    shutil.copy(file, out_dir / name)
 
 
 def copy_to_out(file: Path):
-    shutil.copy(file, out_dir / file.name)
+    copy_to_out_with_name(file, file.name)
 
 
 def copy_to_out_unique(file: Path):
-    dest = out_dir / f'{file.stem}-{unix_time}{file.suffix}'
-    shutil.copy(file, dest)
+    copy_to_out_with_name(file, f'{file.stem}-{unix_time}{file.suffix}')
 
 
 def replace_string(file: Path, init: str, new: str):
@@ -37,18 +33,47 @@ def replace_string(file: Path, init: str, new: str):
         f.write(cont)
 
 
+def compare_files_with_replacement(file1: Path, file2: Path, init: str, new: str) -> bool:
+    with open(file1, 'r') as f1, open(file2, 'r') as f2:
+        return f1.read().replace(init, new) == f2.read()
+
+
 if __name__ == '__main__':
     RELEASE_FILES = ['trace.js', 'index.html', 'init.js', 'trace.css']
     MAKE_UNIQUE = ['worker.js', 'a.out.wasm']
 
+    worker = None
+    wasm = None
+    file_time = unix_time
+    for file in out_dir.iterdir():
+        if 'worker-' in file.stem:
+            worker = file
+            file_time = file.stem.split('-')[1]
+        elif 'a.out-' in file.stem:
+            wasm = file
+
+    new_worker_name = f'worker-{file_time}.js'
+    new_wasm_name = f'a.out-{file_time}.wasm'
+
+    if not (worker is not None and wasm is not None and compare_files_with_replacement(src_dir / 'worker.js', worker, 'a.out.wasm', new_wasm_name) and filecmp.cmp(src_dir / 'a.out.wasm', wasm)):
+        print('New Worker or WASM file detected, replacing with new ones')
+        for item in out_dir.iterdir():
+            try:
+                if item.is_file() or item.is_symlink():
+                    item.unlink()
+            except Exception as e:
+                print(f'Failed to delete {item}. Reason: {e}')
+        for file in MAKE_UNIQUE:
+            copy_to_out_unique(src_dir / file)
+        file_time = unix_time
+    else:
+        print('No new Worker or WASM file detected, keeping old ones')
+
+    new_worker_name = f'worker-{file_time}.js'
+    new_wasm_name = f'a.out-{file_time}.wasm'
+
     for file in RELEASE_FILES:
         copy_to_out(src_dir / file)
-
-    for file in MAKE_UNIQUE:
-        copy_to_out_unique(src_dir / file)
-
-    new_worker_name = f'worker-{unix_time}.js'
-    new_wasm_name = f'a.out-{unix_time}.wasm'
 
     replace_string(out_dir / 'trace.js', 'worker.js', new_worker_name)
     replace_string(out_dir / new_worker_name, 'a.out.wasm', new_wasm_name)
