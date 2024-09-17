907cb250db758708b97286ddd0534dcd64267ee0
usy <contact.usyless@gmail.com>
2024-09-17 19:07:55 +0100

    try fix service worker

--- src/updater.js
+++ src/updater.js
@@ -1,22 +1,24 @@
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.addEventListener('controllerchange', () => {
-        window.location.reload();
-    });
+(() => {
+    if ("serviceWorker" in navigator) {
+        navigator.serviceWorker.addEventListener('controllerchange', () => {
+            window.location.reload();
+        });
 
-    navigator.serviceWorker.register('./service-worker.js').then(registration => {
-        registration.addEventListener('updatefound', () => {
-            const installingWorker = registration.installing;
-            installingWorker.addEventListener('statechange', () => {
-                if (installingWorker.state === 'installed') {
-                    if (navigator.serviceWorker.controller) {
-                        const b = document.getElementById('updateAvailable');
-                        b.addEventListener('click', () => {
-                            installingWorker.postMessage({ action: 'skipWaiting' });
-                        });
-                        b.classList.remove('hidden');
+        navigator.serviceWorker.register('./service-worker.js').then(registration => {
+            registration.addEventListener('updatefound', () => {
+                const installingWorker = registration.installing;
+                installingWorker.addEventListener('statechange', () => {
+                    if (installingWorker.state === 'installed') {
+                        if (navigator.serviceWorker.controller) {
+                            const b = document.getElementById('updateAvailable');
+                            b.addEventListener('click', () => {
+                                installingWorker.postMessage({ action: 'skipWaiting' });
+                            });
+                            b.classList.remove('hidden');
+                        }
                     }
-                }
+                });
             });
         });
-    });
-}
+    }
+})();
