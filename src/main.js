2aaa985b327855752c61d77eb11d0dcbd7563123
usy <contact.usyless@gmail.com>
2024-09-17 19:14:02 +0100

    move service worker initiator back into main js file

--- src/main.js
+++ src/main.js
@@ -1,5 +1,28 @@
 'use strict';
 
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.addEventListener('controllerchange', () => {
+        window.location.reload();
+    });
+
+    navigator.serviceWorker.register('./service-worker.js').then(registration => {
+        registration.addEventListener('updatefound', () => {
+            const installingWorker = registration.installing;
+            installingWorker.addEventListener('statechange', () => {
+                if (installingWorker.state === 'installed') {
+                    if (navigator.serviceWorker.controller) {
+                        const b = document.getElementById('updateAvailable');
+                        b.addEventListener('click', () => {
+                            installingWorker.postMessage({ action: 'skipWaiting' });
+                        });
+                        b.classList.remove('hidden');
+                    }
+                }
+            });
+        });
+    });
+}
+
 // Defaults
 const defaults = {
     "FRHigher": 20000,
