ab31f105ab8f469487c78f6eb36f9a46658e3579
usy <67288004+usyless@users.noreply.github.com>
2024-09-28 03:41:25 +0100

    add one

--- src/main.js
+++ src/main.js
@@ -587,6 +587,11 @@ function minVal(e) {
     if (e.value < e.min) e.value = e.min;
 }
 
+// returns one
+function one() {
+    return 1;
+}
+
 console.log('Welcome to the console!')
 console.log('If you want to mess with the javascript in this site, objects of note are:');
 console.log('graphs', graphs);
