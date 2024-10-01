3d5f2868c40076c1fc50c5630fcf31b7357bb2ba
usy <contact.usyless@gmail.com>
2024-10-01 20:53:25 +0100

    basic tutorial system (will be more modular)

--- src/tutorial.js
+++ src/tutorial.js
@@ -1,15 +1,20 @@
 (() => {
     'use strict';
 
-    const TUTORIAL_VERSION = 1;
+    const TUTORIAL_VERSION = 2;
     const USER_TUTORIAL_VERSION = (() => {
         try {return parseInt(window.localStorage.getItem('USER_TUTORIAL_VERSION') || 0);} catch {return 0;}
     })();
 
-    if (TUTORIAL_VERSION > USER_TUTORIAL_VERSION) {
-        showTutorial();
-        // window.localStorage.setItem('USER_TUTORIAL_VERSION', TUTORIAL_VERSION.toString());
-    }
+    const TUTORIAL_PAGES = [{
+        header: 'Welcome to UsyTrace',
+        body: `This is a website made for you to trace data from images of frequency response graphs.
+The next few pages of the tutorial will show all of the functionality.
+This tutorial will be updated occasionally, and when it is it will be shown to you upon visiting UsyTrace again, in which case I'd recommend reading it again!`
+    }];
+    let CURRENT_PAGE = 0;
+
+    if (TUTORIAL_VERSION !== USER_TUTORIAL_VERSION) showTutorial();
 
     function showTutorial() {
         const center_div = document.createElement('div'),
@@ -37,18 +42,49 @@
         exit.classList.add('standardButton');
         exit.textContent = '𐌢';
         exit.addEventListener('click', hideTutorial);
-        head.textContent = 'Welcome to UsyTrace!';
 
         headContainer.append(head, exit);
-        text_div.appendChild(headContainer);
+
+        const mainText = document.createElement('p');
+        text_div.append(headContainer, mainText);
 
         // Buttons
         const nextPage = document.createElement('button'),
             previousPage = document.createElement('button');
         nextPage.classList.add('standardButton');
         previousPage.classList.add('standardButton');
-        nextPage.textContent = 'Begin Tutorial';
-        previousPage.disabled = true;
+
+        nextPage.addEventListener('click', () => {
+            if (CURRENT_PAGE >= TUTORIAL_PAGES.length - 1) exit.click();
+            ++CURRENT_PAGE;
+            updateButtons();
+        });
+        previousPage.addEventListener('click', () => {
+            --CURRENT_PAGE;
+            updateButtons();
+        });
+        updateButtons();
+        loadPage();
+
+        function updateButtons() {
+            if (CURRENT_PAGE >= TUTORIAL_PAGES.length - 1) nextPage.textContent = "End Tutorial";
+            else if (CURRENT_PAGE <= 0) {
+                nextPage.textContent = "Begin Tutorial";
+                previousPage.disabled = true;
+                previousPage.textContent = "";
+            } else {
+                nextPage.textContent = "Next";
+                previousPage.disabled = false;
+                previousPage.textContent = "Previous";
+            }
+            loadPage();
+        }
+
+        function loadPage() {
+            const data = TUTORIAL_PAGES[CURRENT_PAGE] || TUTORIAL_PAGES[0];
+            head.textContent = data['header'];
+            mainText.textContent = data['body'];
+        }
 
         buttons_div.append(previousPage, nextPage);
 
@@ -57,7 +93,9 @@
     }
 
     function hideTutorial() {
+        CURRENT_PAGE = 0;
         Popups.clearPopups();
+        window.localStorage.setItem('USER_TUTORIAL_VERSION', TUTORIAL_VERSION.toString())
     }
 
     document.getElementById('tutorial').addEventListener('click', showTutorial);
