a2ab79aa39ed3c6416b0122cfc1a816afb7cca45
usy <contact.usyless@gmail.com>
2024-10-11 17:34:41 +0100

    cleaner tutorial

--- src/tutorial.js
+++ src/tutorial.js
@@ -3,40 +3,55 @@
 
     const TUTORIAL_VERSION = 1;
     const USER_TUTORIAL_VERSION = (() => {
-        try {return parseInt(window.localStorage.getItem('USER_TUTORIAL_VERSION') || 0);} catch {return 0;}
+        try {return parseInt(window.localStorage.getItem('USER_TUTORIAL_VERSION') ?? 0);} catch {return 0;}
     })();
 
     const TUTORIAL_PAGES = [{
         header: 'Welcome to UsyTrace',
-        body: `This is a website made for you to trace data from images of frequency response graphs.
-The next few pages of the tutorial will show all of the functionality.
-This tutorial will be updated occasionally, and when it is it will be shown to you upon visiting UsyTrace again, in which case I'd recommend reading it again!`
+        body: `Here you can trace frequency responses from images.`
     }, {
         header: 'Tracing',
-        body: `Auto Trace: Tries to automatically select a line in the image, may fail, applied automatically upon image import
-Select Path: Traces to the right of the point you click on the image, can complete a trace by starting from the left side of the frequency response and clicking to the right if it misses a part
-Add Point: Add a single point to the trace manually, useful for situations where Select Path or Auto Trace Fails
-Undo: Goes back to the previous trace step, saves add point, tracing, and clearing history
-Clear Path: Clears the current trace on screen, can go back using Undo`
+        body: `<h3>Auto Trace</h3>
+Tries to automatically select a line in the image, may fail, applied automatically upon image import
+        
+<h3>Select Path</h3>
+Traces to the right of the point you click on the image, can complete a trace by starting from the left side of the frequency response and clicking to the right if it misses a part
+
+<h3>Add Point</h3>
+Add a single point to the trace manually, useful for situations where Select Path or Auto Trace Fails
+
+<h3>Undo</h3>
+Goes back to the previous trace step, saves add point, tracing, and clearing history
+
+<h3>Clear Path</h3>
+Clears the current trace on screen, can go back using Undo`
     }, {
         header: 'Exporting',
-        body: `Align the two sets of "High" and "Low" lines with a given value in their axis, the specific value does not matter, then input the respective values into "Higher SPL", "Lower SPL", and adjust the Frequency values if not using 20Hz and 20000Hz
-Adjust the minimum and maximum exported value with "Minimum Frequency" and "Maximum Frequency", as well as this, you can adjust the delimitation and points per octave (PPO) of the exported data in the "Trace Settings"
-Then "Export Trace" will give you the result`
+        body: `Align <b>High</b> and <b>Low</b> lines with given values in their respective axis, then input the respective values into <b>Higher SPL</b>, <b>Lower SPL</b>, <b>Higher Frequency</b> and <b>Lower Frequency</b>.
+
+<h3>Trace Settings</h3>     
+Adjust the <b>Minimum Exported Frequency</b>, <b>Maximum Exported Frequency</b>, <b>Points Per Octave of the exported data</b>, and <b>the delimination of the exported data</b>
+
+Then <b>Export Trace</b> will give you the result`
     }, {
         header: 'Settings',
-        body: `Buttons Snap To Axis: whether the buttons underneath the Export values will attempt to snap the lines to their axis, or to enable smooth manual movement
-Colour Tolerance: Raise if line is not being detected, Lower if line is jagged or detecting multiple lines`
+        body: `<h3>Buttons Snap To Axis</h3>
+whether the buttons underneath the Export values will attempt to snap the lines to their axis, or to enable smooth manual movement
+        
+<h3>Colour Tolerance</h3>
+Raise if line is not being detected, Lower if line is jagged or detecting multiple lines`
     }, {
         header: 'Miscellaneous',
-        body: `You can drop, or import any amount of images you like into the site, other than with pasting, as that only supports one image at a time.
-Loaded images show in the image queue on the bottom, and can be removed by right clicking them (long hold on mobile) or pressing remove`
+        body: `You can <b>drop</b>, or <b>choose any amount of images</b> you like into the site, other than with pasting, as that only supports one image at a time.
+        
+Loaded images show in the image queue on the bottom, and can be removed by right-clicking them (long hold on mobile) or pressing remove`
     }];
     let CURRENT_PAGE = 0;
 
     if (TUTORIAL_VERSION !== USER_TUTORIAL_VERSION) showTutorial();
 
     function showTutorial() {
+        CURRENT_PAGE = 0;
         const center_div = document.createElement('div'),
             main_div = document.createElement('div'),
             inner_div = document.createElement('div'),
@@ -45,14 +60,13 @@ Loaded images show in the image queue on the bottom, and can be removed by right
 
         center_div.setAttribute('usy-overlay', '');
         center_div.classList.add('fullscreen');
-        main_div.classList.add('popupOuter');
-        inner_div.classList.add('popupInner');
+        main_div.classList.add('popupOuter', 'fixedSize');
+        inner_div.classList.add('popupInner', 'fixedSize');
         buttons_div.classList.add('popupButtons');
         text_div.classList.add('popupText');
 
         center_div.appendChild(main_div);
         main_div.appendChild(inner_div);
-        inner_div.append(text_div, buttons_div);
 
         // Main Page
         const headContainer = document.createElement('div'),
@@ -64,9 +78,10 @@ Loaded images show in the image queue on the bottom, and can be removed by right
         exit.addEventListener('click', hideTutorial);
 
         headContainer.append(head, exit);
+        inner_div.append(headContainer, text_div, buttons_div);
 
-        const mainText = document.createElement('h4');
-        text_div.append(headContainer, mainText);
+        const mainText = document.createElement('p');
+        text_div.append(mainText);
 
         // Buttons
         const nextPage = document.createElement('button'),
@@ -101,9 +116,9 @@ Loaded images show in the image queue on the bottom, and can be removed by right
         }
 
         function loadPage() {
-            const data = TUTORIAL_PAGES[CURRENT_PAGE] || TUTORIAL_PAGES[0];
+            const data = TUTORIAL_PAGES[CURRENT_PAGE] ?? TUTORIAL_PAGES[0];
             head.textContent = data['header'];
-            mainText.textContent = data['body'];
+            mainText.innerHTML = data['body'];
         }
 
         buttons_div.append(previousPage, nextPage);
@@ -113,7 +128,6 @@ Loaded images show in the image queue on the bottom, and can be removed by right
     }
 
     function hideTutorial() {
-        CURRENT_PAGE = 0;
         Popups.clearPopups();
         window.localStorage.setItem('USER_TUTORIAL_VERSION', TUTORIAL_VERSION.toString());
     }
