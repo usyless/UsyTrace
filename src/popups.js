371f85b5375dda2af98e82552f117bfb15268b7f
usy <contact.usyless@gmail.com>
2025-01-07 00:21:53 +0000

    add custom text element support for popup

--- src/popups.js
+++ src/popups.js
@@ -1,45 +1,35 @@
 'use strict';
 
-/**
- * Create a custom popup, that will go away once "ok" is clicked
- * @param textContent text to be shown inside the popup
- * @param confirmation whether to add cancel button that returns false
- */
-export async function createPopup(textContent, confirmation = false) {
+export async function createPopup(textContent, customTextElem) {
     clearPopups();
     return new Promise((resolve) => {
         const center_div = document.createElement('div'),
             main_div = document.createElement('div'),
             inner_div = document.createElement('div'),
-            text_div = document.createElement('div'),
             buttons_div = document.createElement('div'),
-            ok_button = document.createElement('button'),
-            cancel_button = document.createElement('button');
+            ok_button = document.createElement('button');
+        let text_div = document.createElement('div');
 
         center_div.setAttribute('usy-overlay', '');
         center_div.classList.add('fullscreen', 'blur');
         main_div.classList.add('popupOuter');
         inner_div.classList.add('popupInner');
         buttons_div.classList.add('popupButtons');
-        text_div.classList.add('popupText');
+        if (textContent != null) {
+            text_div.classList.add('popupText');
+            text_div.textContent = textContent;
+        } else text_div = customTextElem;
         ok_button.classList.add('standardButton');
-        cancel_button.classList.add('standardButton');
 
         ok_button.textContent = 'Ok';
-        cancel_button.textContent = 'Cancel';
 
         main_div.appendChild(inner_div);
         inner_div.append(text_div, buttons_div);
         buttons_div.appendChild(ok_button);
-        if (confirmation) buttons_div.appendChild(cancel_button);
 
         ok_button.addEventListener('click', () => {
             clearPopups();
             resolve(true);
-        });
-        cancel_button.addEventListener('click', () => {
-            clearPopups();
-            resolve(false);
         });
         center_div.addEventListener('click', () => {
             clearPopups();
@@ -49,8 +39,6 @@ export async function createPopup(textContent, confirmation = false) {
             e.stopPropagation();
             e.stopImmediatePropagation();
         });
-
-        text_div.textContent = textContent;
 
         center_div.appendChild(main_div);
 
